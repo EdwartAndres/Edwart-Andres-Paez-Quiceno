f135d3633b8ec66076c4c4efb70231f789ea1e9e
EdwartAndres <125517012+EdwartAndres@users.noreply.github.com>
2023-05-03 00:14:23 -0500

    Homework third class

---
+++ dino_runner/components/obstacles/bird.py
@@ -0,0 +1,10 @@
+import random
+
+from dino_runner.components.obstacles.obstacle import Obstacle
+
+class Bird(Obstacle):
+
+    def __init__(self, image):
+        self.height_position = random.randint(0, 2)
+        super().__init__(image, 3)
+        self.rect.y = 320 - (self.height_position * 60)
